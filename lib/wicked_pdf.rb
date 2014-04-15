ed321de884b5d8a40d36c8bef424015f5f8d6304
tdenovan <theo@t6d.com>
2014-04-15 12:22:05 +1000

    Update wicked_pdf.rb
    added image from pdf

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -77,6 +77,39 @@ class WickedPdf
     generated_pdf_file.close! if generated_pdf_file && !return_file
   end
 
+  # Modifications by tdenovan to support image rendering
+  # TODO: refactor this method and pdf_from_string to reuse shared code
+  def image_from_string(string, options={})
+    if WickedPdf.config[:retreive_version]
+      retreive_binary_version
+    end
+
+    temp_path = options.delete(:temp_path)
+    string_file = WickedPdfTempfile.new("wicked_pdf.html", temp_path)
+    string_file.binmode
+    string_file.write(string)
+    string_file.close
+    generated_pdf_file = WickedPdfTempfile.new("wicked_pdf_generated_file.png", temp_path)
+    command = "\"#{options[:wkhtmltoimage]}\" -f png #{parse_options(options)} \"file:///#{string_file.path}\" \"#{generated_pdf_file.path}\" " # -q for no errors on stdout
+    print_command(command) if in_development_mode?
+    err = Open3.popen3(command) do |stdin, stdout, stderr|
+      stderr.read
+    end
+    if return_file = options.delete(:return_file)
+      return generated_pdf_file
+    end
+    generated_pdf_file.rewind
+    generated_pdf_file.binmode
+    pdf = generated_pdf_file.read
+    raise "PDF could not be generated!\n Command Error: #{err}" if pdf and pdf.rstrip.length == 0
+    pdf
+  rescue Exception => e
+    raise "Failed to execute:\n#{command}\nError: #{e}"
+  ensure
+    string_file.close! if string_file
+    generated_pdf_file.close! if generated_pdf_file && !return_file
+  end
+
   private
 
     def in_development_mode?
@@ -217,6 +250,8 @@ class WickedPdf
                                     :page_size,
                                     :page_width,
                                     :page_height,
+                                    :width,
+                                    :height,
                                     :proxy,
                                     :username,
                                     :password,
